772342e7277db3d7c166234b40d4ad2ca141c516
M Alif Hasan <153391891+alif00007@users.noreply.github.com>
2026-05-19 07:02:02 +0600

    Add files via upload

---
+++ Attendance.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Gym_Management_System
+{
+    partial class Attendance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MemberCb = new System.Windows.Forms.ComboBox();
+            this.btnCheckIn = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AttendanceDGV = new System.Windows.Forms.DataGridView();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.AttendanceDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MemberCb
+            // 
+            this.MemberCb.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MemberCb.FormattingEnabled = true;
+            this.MemberCb.Location = new System.Drawing.Point(412, 146);
+            this.MemberCb.Name = "MemberCb";
+            this.MemberCb.Size = new System.Drawing.Size(163, 41);
+            this.MemberCb.TabIndex = 0;
+            // 
+            // btnCheckIn
+            // 
+            this.btnCheckIn.BackColor = System.Drawing.Color.Crimson;
+            this.btnCheckIn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCheckIn.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheckIn.ForeColor = System.Drawing.Color.White;
+            this.btnCheckIn.Location = new System.Drawing.Point(581, 146);
+            this.btnCheckIn.Name = "btnCheckIn";
+            this.btnCheckIn.Size = new System.Drawing.Size(176, 42);
+            this.btnCheckIn.TabIndex = 1;
+            this.btnCheckIn.Text = "Check In";
+            this.btnCheckIn.UseVisualStyleBackColor = false;
+            this.btnCheckIn.Click += new System.EventHandler(this.btnCheckIn_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.BackColor = System.Drawing.Color.Crimson;
+            this.btnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRefresh.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefresh.Location = new System.Drawing.Point(763, 146);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(152, 42);
+            this.btnRefresh.TabIndex = 2;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = false;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.Crimson;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(412, 639);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(110, 52);
+            this.btnBack.TabIndex = 3;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Teal;
+            this.label1.Location = new System.Drawing.Point(367, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 39);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Attendance";
+            // 
+            // AttendanceDGV
+            // 
+            this.AttendanceDGV.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.AttendanceDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.AttendanceDGV.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.AttendanceDGV.Location = new System.Drawing.Point(12, 210);
+            this.AttendanceDGV.Name = "AttendanceDGV";
+            this.AttendanceDGV.RowHeadersWidth = 51;
+            this.AttendanceDGV.RowTemplate.Height = 24;
+            this.AttendanceDGV.Size = new System.Drawing.Size(903, 409);
+            this.AttendanceDGV.TabIndex = 5;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Century Gothic", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Crimson;
+            this.label9.Location = new System.Drawing.Point(900, 10);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(37, 38);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "X";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // Attendance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 719);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.AttendanceDGV);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnCheckIn);
+            this.Controls.Add(this.MemberCb);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Attendance";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Attendance";
+            this.Load += new System.EventHandler(this.Attendance_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.AttendanceDGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox MemberCb;
+        private System.Windows.Forms.Button btnCheckIn;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView AttendanceDGV;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ Attendance.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Gym_Management_System
+{
+    public partial class Attendance : Form
+    {
+        public Attendance()
+        {
+            InitializeComponent();
+        }
+
+        // DATABASE CONNECTION
+        SqlConnection Con = new SqlConnection(
+            @"Data Source=ALIF\SQLEXPRESS;
+              Initial Catalog=GymDb;
+              Integrated Security=True;
+              TrustServerCertificate=True");
+
+        // LOAD MEMBER NAMES
+        private void FillMembers()
+        {
+            try
+            {
+                if (Con.State == ConnectionState.Closed)
+                {
+                    Con.Open();
+                }
+
+                string query;
+
+                // MEMBER CAN SEE ONLY OWN NAME
+                if (Login.UserRole == "Member")
+                {
+                    query =
+                    @"SELECT MName
+                      FROM MemberTbl
+                      WHERE MName=@Name";
+                }
+                else
+                {
+                    // ADMIN CAN SEE ALL MEMBERS
+                    query =
+                    @"SELECT MName
+                      FROM MemberTbl";
+                }
+
+                SqlCommand cmd =
+                    new SqlCommand(query, Con);
+
+                // PASS LOGGED USER
+                if (Login.UserRole == "Member")
+                {
+                    cmd.Parameters.AddWithValue(
+                        "@Name",
+                        Login.LoggedUser);
+                }
+
+                SqlDataReader rdr =
+                    cmd.ExecuteReader();
+
+                DataTable dt = new DataTable();
+
+                dt.Columns.Add("MName", typeof(string));
+
+                dt.Load(rdr);
+
+                // COMBOBOX SETTINGS
+                MemberCb.DisplayMember = "MName";
+
+                MemberCb.ValueMember = "MName";
+
+                MemberCb.DataSource = dt;
+
+                // MEMBER AUTO SELECT
+                if (Login.UserRole == "Member")
+                {
+                    MemberCb.SelectedIndex = 0;
+
+                    MemberCb.Enabled = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Con.Close();
+            }
+        }
+
+        // SHOW ATTENDANCE
+        private void populate()
+        {
+            try
+            {
+                if (Con.State == ConnectionState.Closed)
+                {
+                    Con.Open();
+                }
+
+                string query;
+
+                // MEMBER CAN SEE ONLY OWN ATTENDANCE
+                if (Login.UserRole == "Member")
+                {
+                    query =
+                    @"SELECT *
+                      FROM AttendanceTbl
+                      WHERE MemberName=@Name";
+                }
+                else
+                {
+                    // ADMIN CAN SEE ALL
+                    query =
+                    @"SELECT *
+                      FROM AttendanceTbl";
+                }
+
+                SqlDataAdapter sda =
+                    new SqlDataAdapter(query, Con);
+
+                // PASS LOGGED USER
+                if (Login.UserRole == "Member")
+                {
+                    sda.SelectCommand.Parameters.AddWithValue(
+                        "@Name",
+                        Login.LoggedUser);
+                }
+
+                DataTable dt = new DataTable();
+
+                sda.Fill(dt);
+
+                AttendanceDGV.DataSource = dt;
+
+                // GRID DESIGN
+                AttendanceDGV.BorderStyle =
+                    BorderStyle.None;
+
+                AttendanceDGV.BackgroundColor =
+                    Color.White;
+
+                AttendanceDGV.AutoSizeColumnsMode =
+                    DataGridViewAutoSizeColumnsMode.Fill;
+
+                AttendanceDGV.RowHeadersVisible = false;
+
+                AttendanceDGV.SelectionMode =
+                    DataGridViewSelectionMode.FullRowSelect;
+
+                AttendanceDGV.AllowUserToAddRows = false;
+
+                AttendanceDGV.EnableHeadersVisualStyles = false;
+
+                // HEADER STYLE
+                AttendanceDGV.ColumnHeadersDefaultCellStyle.BackColor =
+                    Color.Crimson;
+
+                AttendanceDGV.ColumnHeadersDefaultCellStyle.ForeColor =
+                    Color.White;
+
+                AttendanceDGV.ColumnHeadersDefaultCellStyle.Font =
+                    new Font("Century Gothic", 11, FontStyle.Bold);
+
+                // CELL STYLE
+                AttendanceDGV.DefaultCellStyle.BackColor =
+                    Color.White;
+
+                AttendanceDGV.DefaultCellStyle.ForeColor =
+                    Color.Black;
+
+                AttendanceDGV.DefaultCellStyle.SelectionBackColor =
+                    Color.DodgerBlue;
+
+                AttendanceDGV.DefaultCellStyle.SelectionForeColor =
+                    Color.White;
+
+                AttendanceDGV.DefaultCellStyle.Font =
+                    new Font("Century Gothic", 10);
+
+                AttendanceDGV.RowTemplate.Height = 30;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Con.Close();
+            }
+        }
+
+        // FORM LOAD
+        private void Attendance_Load(
+            object sender,
+            EventArgs e)
+        {
+            FillMembers();
+
+            populate();
+
+            // FORM COLOR
+            this.BackColor = Color.WhiteSmoke;
+
+            // BUTTON DESIGN
+            btnCheckIn.FlatStyle = FlatStyle.Flat;
+            btnRefresh.FlatStyle = FlatStyle.Flat;
+            btnBack.FlatStyle = FlatStyle.Flat;
+
+            btnCheckIn.BackColor = Color.Crimson;
+            btnRefresh.BackColor = Color.Crimson;
+            btnBack.BackColor = Color.Crimson;
+
+            btnCheckIn.ForeColor = Color.White;
+            btnRefresh.ForeColor = Color.White;
+            btnBack.ForeColor = Color.White;
+
+            // COMBOBOX DESIGN
+            MemberCb.Font =
+                new Font("Century Gothic", 11);
+
+            MemberCb.ForeColor =
+                Color.Black;
+
+            MemberCb.BackColor =
+                Color.White;
+        }
+
+        // CHECK IN BUTTON
+        private void btnCheckIn_Click(
+            object sender,
+            EventArgs e)
+        {
+            if (MemberCb.Text == "")
+            {
+                MessageBox.Show(
+                    "Select Member");
+            }
+            else
+            {
+                try
+                {
+                    if (Con.State == ConnectionState.Closed)
+                    {
+                        Con.Open();
+                    }
+
+                    // CHECK DUPLICATE ENTRY
+                    string checkQuery =
+                    @"SELECT COUNT(*)
+                      FROM AttendanceTbl
+                      WHERE MemberName=@Name
+                      AND AttendDate=@Date";
+
+                    SqlCommand checkCmd =
+                        new SqlCommand(checkQuery, Con);
+
+                    checkCmd.Parameters.AddWithValue(
+                        "@Name",
+                        MemberCb.Text);
+
+                    checkCmd.Parameters.AddWithValue(
+                        "@Date",
+                        DateTime.Today);
+
+                    int count =
+                        Convert.ToInt32(
+                            checkCmd.ExecuteScalar());
+
+                    if (count > 0)
+                    {
+                        MessageBox.Show(
+                            "Already Checked In Today");
+                    }
+                    else
+                    {
+                        string query =
+                        @"INSERT INTO AttendanceTbl
+                        (
+                            MemberName,
+                            AttendDate
+                        )
+                        VALUES
+                        (
+                            @Name,
+                            @Date
+                        )";
+
+                        SqlCommand cmd =
+                            new SqlCommand(query, Con);
+
+                        cmd.Parameters.AddWithValue(
+                            "@Name",
+                            MemberCb.Text);
+
+                        cmd.Parameters.AddWithValue(
+                            "@Date",
+                            DateTime.Today);
+
+                        cmd.ExecuteNonQuery();
+
+                        MessageBox.Show(
+                            "Attendance Added Successfully");
+
+                        populate();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    Con.Close();
+                }
+            }
+        }
+
+        // REFRESH BUTTON
+        private void btnRefresh_Click(
+            object sender,
+            EventArgs e)
+        {
+            populate();
+
+            MessageBox.Show(
+                "Attendance Refreshed");
+        }
+
+        // BACK BUTTON
+        private void btnBack_Click(
+            object sender,
+            EventArgs e)
+        {
+            MainForm mainform =
+                new MainForm();
+
+            mainform.Show();
+
+            this.Hide();
+        }
+
+        // EXIT LABEL
+        private void label9_Click(
+            object sender,
+            EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
